3531a43336b8a4ec0500c13f4f2bfa3a9d20098d
fromentxavier <fromentxavier@gmail.com>
2021-05-08 23:35:03 +0200

    minor fixes

--- src/components/system/RoutePaths.js
+++ src/components/system/RoutePaths.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import { DataProvider } from '../data/DataProvider'
 // System
 import Error from './Error'
