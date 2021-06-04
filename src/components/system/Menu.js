d2f3047dc7334b9829a7646c66631ef759e708da
fromentxavier <fromentxavier@gmail.com>
2021-06-04 11:38:23 +0200

    adding collapse on select in menu, for phones POV

--- src/components/system/Menu.js
+++ src/components/system/Menu.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import {Navbar, Nav} from 'react-bootstrap'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { DataContext } from '../data/DataProvider'
 import './Menu.css'
 
@@ -11,19 +11,71 @@ function Menu() {
 
   return (
     <>
-      <Navbar bg="light" variant="light" expand="lg" sticky="top" className="shadow-sm">
+      <Navbar collapseOnSelect bg="light" variant="light" expand="lg" sticky="top" className="shadow-sm">
         <Navbar.Brand>MousePAD</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="m-auto text-center">
-            <NavLink className="nav-link menu-hover" to="/" end>Accueil</NavLink>
-            <NavLink className="nav-link menu-hover" to="/about">A propos</NavLink>       
-            <NavLink className="nav-link menu-hover" to="/products">Produits</NavLink>
-            <NavLink className="nav-link menu-hover" to="/contact">Contact</NavLink>            
-          </Nav>
-          <Nav className="ml-auto text-center">
-            <NavLink className="nav-link menu-hover" to="/account">Compte</NavLink>  
-            <NavLink className="nav-link menu-hover" to="/cart">Panier {cart.length > 0 && (<span className="badge badge-danger">{cart.length}</span>)}</NavLink>  
+            <Nav.Item>
+              <Nav.Link 
+                eventKey="1" 
+                as={Link} 
+                className="nav-link menu-hover" 
+                to="/" end="true"
+              >
+                Accueil
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link 
+                eventKey="2" 
+                as={Link} 
+                className="nav-link menu-hover" 
+                to="/about"
+              >
+                A propos
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link 
+                eventKey="3" 
+                as={Link} 
+                className="nav-link menu-hover" 
+                to="/products"
+              >
+                Produits
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link 
+                eventKey="4" 
+                as={Link} 
+                className="nav-link menu-hover" 
+                to="/contact"
+              >
+                Contact
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link 
+                eventKey="5" 
+                as={Link} 
+                className="nav-link menu-hover" 
+                to="/account"
+              >
+                Compte
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link 
+                eventKey="6" 
+                as={Link} 
+                className="nav-link menu-hover" 
+                to="/cart"
+              >
+                Panier {cart.length > 0 && (<span className="badge badge-danger">{cart.length}</span>)}
+              </Nav.Link>
+            </Nav.Item>            
           </Nav>
         </Navbar.Collapse>
       </Navbar>
